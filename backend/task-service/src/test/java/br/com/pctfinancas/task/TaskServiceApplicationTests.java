4f965a9d7fb3fa750884b6ecec2a55f4a1a58e2e
Victor Couto <victorcto@outlook.com>
2022-10-30 09:00:33 -0300

    Criação do projeto e do task-service

---
+++ backend/task-service/src/test/java/br/com/pctfinancas/task/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pctfinancas.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
