96ec5894956340ea6decfec6002584b6e22a00cf
Victor Couto <victorcto@outlook.com>
2022-12-03 22:47:27 -0300

    autenticação via banco de dados

---
+++ backend/task-service/src/main/java/br/com/reorganize/task/entities/User.java
@@ -0,0 +1,114 @@
+package br.com.reorganize.task.entities;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "usuario")
+public class User implements UserDetails{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome_usuario", nullable = false, unique = true)
+	private String username;
+	
+	@Column(name = "senha", nullable = false)
+	private String password;
+	
+	public User() {
+		
+	}
+	
+	public User(Long id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(username, other.username);
+	}
+
+	@Override
+	public String toString() {
+		return "User " + id + "\n\tNome de usuário = " + username + "\n\tSenha = " + password;
+	}
+	
+}
---
+++ backend/task-service/src/main/java/br/com/reorganize/task/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.reorganize.task.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.reorganize.task.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+}
